a580d43078294e7894e0ce30c0a96475a21d38e1
Peter Kalavritinos <peterkalav1994@gmail.com>
2021-11-29 11:52:56 -0500

    Upload from sandbox to github

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var footer = document.querySelector(".contact");
+footer.style.background = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "Hello World!";
+
+var address = document.querySelector("address");
+address.style.fontSize = "1.75";
+address.style.lineHeight = "1.5em";
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day </em>  of the week.";
+
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+
+var openingHours = document.querySelector(".contact p");
+openingHours.style.border = "1px solid #d45bb9";
+openingHours.style.padding = "1em";
